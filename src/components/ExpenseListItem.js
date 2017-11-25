5cd457796ea0ab6d953fd0b52aa35d40a44d7870
Abdullah Alhassan <aalhassan2@zagmail.gonzaga.edu>
2017-11-25 05:34:37 -0800

    setup the number formatter for showing money

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -2,13 +2,13 @@ import React from 'react'
 import {Link} from 'react-router-dom'
 import moment from 'moment'
 import {connect} from 'react-redux'
-
+import numeral from 'numeral'
 export const ExpenseListItem = ({id, description, amount, createdAt}) => (
   <div>
       <Link to={`/edit/${id}`}>
         <h3>{description}</h3>
       </Link>
-      <p>${parseFloat(amount/100, 10)} - {moment(createdAt).format('MMM Do, YYYY')}</p>
+      <p>{numeral(amount/100).format('$0,0.00')} - {moment(createdAt).format('MMM Do, YYYY')}</p>
 
   </div>
 );
